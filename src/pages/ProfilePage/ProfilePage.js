abe7f4285f558b4230dd6993b2324192492d35a4
chelsea barocio <barocio.chelsea@gmail.com>
2017-09-08 10:19:38 -0700

    made a note in profile page

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -6,6 +6,7 @@ const ProfilePage = (props) => {
     return (
     <div>
         <div>
+            <h1 className='profile-text'> Nothing to See here... yet!</h1>
         <Link className='Settings-cancel btn btn-primary back-home' to='/'>back to home</Link>
       </div>
     </div>

f82e354d0e67fe32913a800c50067acca53489ad
chelsea barocio <barocio.chelsea@gmail.com>
2017-09-08 04:47:14 -0700

    styling changes to mainpage

--- src/components/Twitter/Twitter.js
+++ src/components/Twitter/Twitter.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Twitter.css';
 
 
 
 const Twitter = (props) => {
     return (
-        <div>
+        <div className='Twitter'>
         
-            <a href="https://twitter.com/whittierpd" className="twitter-follow-button" data-show-count="false">Follow @whittierpd</a>
+            <a href="https://twitter.com/whittierpd" className="twitter-follow-button" data-show-count="false">Follow @whittierpd on Twitter!</a>
             <script async src="//platform.twitter.com/widgets.js" charSet="utf-8"></script>
    
         </div>
